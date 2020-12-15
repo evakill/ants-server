6ff9c682a598056b28ffdb8441fc25c5da86ef1a
oodwyer <orodwyer@gmail.com>
2020-12-15 13:30:29 -0500

    followedOrgs route

--- routes/orgs.js
+++ routes/orgs.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var Org = require('../models/Org.js')
+var User = require('../models/User.js')
 
 // get org object from id param
 router.get('/:id', async (req, res, next) => {
@@ -24,6 +25,36 @@ router.get('/', async (req, res, next) => {
         return next({ status: 500, message: 'Error getting org' })
     }
 })
+
+//get followed orgs
+router.get('/followed/:username', async (req, res, next) => {
+    //get user's followed list of ids
+    const userParam = req.params.username;
+    try {
+        console.log(userParam);
+        const user = await User.findOne({username: userParam});
+        const followedList = user.followedOrgs;
+        console.log(followedList);
+    
+        try {
+            //get orgs 
+            const org = await Org.find({});
+            //filter orgs by ones user follows
+            var orgs = org.filter(function (o) {
+                return followedList.includes(o._id);
+            });
+            console.log("followed orgs: " + orgs);
+            res.send({ data: orgs })
+        } catch (err) {
+            console.log("error!");
+            return next({ status: 500, message: 'Error getting org first' })
+        }
+    } catch (err) {
+        console.log("error!");
+        return next({ status: 500, message: 'Error getting org second' })
+    }
+})
+
 
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -14,5 +14,54 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
+//follow org 
+router.post('/followOrg', async (req, res, next) => {
+    const { username, orgID } = req.body;
+    console.log(username + "," + orgID);
+    try {
+        const user = await User.findOne({username: username});
+        console.log("user: " + user);
+        const followedList = user.followedOrgs;
+        console.log(followedList);
+        if (!followedList.includes(orgID)) {
+            followedList.push(orgID) 
+            updatedAccount = await User.findOneAndUpdate({username: username}, { $set: { followedOrgs: followedList}});
+            return res.send({ account: updatedAccount });
+        } else {
+            return res.send({account: user});
+        }
+    } catch (err) {
+        return next({ status: 500, message: 'Error following org' })
+    }
+})
+
+//unfollow org 
+router.post('/unfollowOrg', async (req, res, next) => {
+    const { username, orgID } = req.body;
+    console.log(username + "," + orgID);
+
+    try {
+        const user = await User.findOne({username: username});
+        const followedList = user.followedOrgs;
+        console.log(followedList);
+        if (followedList.includes(orgID)) {
+            console.log(followedList);
+            followedList.splice(followedList.indexOf(orgID), 1);
+            // followedList.filter(x => !x.equals(orgID));
+            // followedList.push(orgID);
+
+            console.log(followedList);
+            updatedAccount = await User.findOneAndUpdate({username: username}, { $set: { followedOrgs: followedList}});
+            return res.send({ account: updatedAccount });
+        } else {
+            return res.send({account: user});
+        }
+    } catch (err) {
+        return next({ status: 500, message: 'Error unfollowing org' })
+    }
+})
+
+//getIsFollowed
+
 
 module.exports = router
